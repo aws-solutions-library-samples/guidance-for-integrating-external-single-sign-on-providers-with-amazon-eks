e6008c0c9e8ecd04fca0762e5712e3429cfff093
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2024-11-13 09:46:08 -0800

    Update configure_kubeconfig.sh
    Obfuscated OIDC issuer URL and Client ID

--- source/configure_kubeconfig.sh
+++ source/configure_kubeconfig.sh
@@ -1,8 +1,9 @@
+# provide actual values for OIDC issuer URL and Client ID
 kubectl config set-credentials oidc \
       --exec-api-version=client.authentication.k8s.io/v1beta1 \
       --exec-command=kubectl \
       --exec-arg=oidc-login \
       --exec-arg=get-token \
-      --exec-arg=--oidc-issuer-url=https://dev-58749656.okta.com/oauth2/aushzoh1hhZLz3coX5d7 \
-      --exec-arg=--oidc-client-id=0oahzoh66sy1pZlTL5d7 \
+      --exec-arg=--oidc-issuer-url=https://dev-XXXXXX.okta.com/oauth2/XXXXXXXXXXXXXXXXXX \
+      --exec-arg=--oidc-client-id=XXXXXXXXXXXXXXXXXX \
       --exec-arg=--oidc-extra-scope="email offline_access profile openid"
